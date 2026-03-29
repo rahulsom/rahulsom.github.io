768ed2d9660c438705953596a1a81c7bb8e27fbf
Rahul Somasunderam <rahul.som@gmail.com>
2026-03-29 15:40:28 -0700

    chore: Use chrome to generate PDF
    Earlier, we used xhtmlrenderer and the quality was lacking.

--- gradle/build-plugin/build.gradle.kts
+++ gradle/build-plugin/build.gradle.kts
@@ -7,8 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jsoup:jsoup:1.22.1")
-    implementation("org.xhtmlrenderer:flying-saucer-pdf:10.2.0")
+    implementation(libs.selenium.java)
 }
 
 gradlePlugin {
